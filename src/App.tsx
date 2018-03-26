26d30ea01a5b7aeb3a2ad4c471b882541bff3bac
olsonek <edward.k.olson@gmail.com>
2018-03-25 21:32:30 -0700

    Moved the provider into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,26 @@ import * as React from 'react';
 import './App.css';
 import StatView from './scenes/StatView';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import configureStore from './store/configureStore';
+import initialState from './reducers/initialState';
+import { Provider } from 'react-redux';
 
 const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <StatView />
-        </div>
-      </MuiThemeProvider>
+      <Provider store={configureStore(initialState)}>
+        <MuiThemeProvider>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <StatView />
+          </div>
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import configureStore from './store/configureStore';
-import initialState from './reducers/initialState';
 
 import './index.css';
 
 ReactDOM.render(
-  <Provider store={configureStore(initialState)}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
